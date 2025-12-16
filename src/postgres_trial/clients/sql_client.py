5cb54b17ee6d744570a786a0e5de2e708c319547
anujachaitanya <anujachaitanya09@gmail.com>
2025-12-16 11:33:04 +0530

    Updated postgresClient

--- src/postgres_trial/clients/sql_client.py
+++ src/postgres_trial/clients/sql_client.py
@@ -4,11 +4,15 @@ from sqlalchemy.orm import sessionmaker
 
 
 class PostgresClient:
+    def __init__(self):
+        self.session = None
+        self.engine = None
+
     def init(self, host, database, user, password, port=5432):
         connection_string = f"postgresql://{user}:{password}@{host}:{port}/{database}"
         self.engine = create_engine(connection_string)
-        Session = sessionmaker(bind=self.engine)
-        self.session = Session()
+        session = sessionmaker(bind=self.engine)
+        self.session = session()
 
     def fetch(self, query, params=None):
         result = self.session.execute(text(query), params or {})
